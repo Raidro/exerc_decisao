7fcf33776024d4f0984a34c3f6b225b763c69301
Mateus <mateus_sat@hotmail.com>
2019-07-04 20:58:22 -0300

    add comentatio

--- respostas-exercicios/resposta10.js
+++ respostas-exercicios/resposta10.js
@@ -1,8 +1,8 @@
 
 //Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
 //Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
-
-let turno = prompt('Qual o seu turno: M- matutino ou V- Vespertino ou N- Noturno?: ').toLowerCase();
+// trabalho com tudo minusculo 
+let turno = prompt('Qual o seu turno: M- matutino ou V- Vespertino ou N- Noturno?: ').toLowerCase().trim();
 
 
 if (turno == 'm') {
