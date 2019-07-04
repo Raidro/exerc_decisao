d1b9190f1c476088b4f2cc6e621007d1e365f86a
Mateus <mateus_sat@hotmail.com>
2019-07-04 20:10:28 -0300

    adicionado comentario

--- respostas-exercicios/resposta6.js
+++ respostas-exercicios/resposta6.js
@@ -1,6 +1,28 @@
 
 //Faça um Programa que leia três números e mostre o maior deles.
 
+/*
+outra forma de resolver essa questão é:
+
+let numero 1 = parseFloat(prompt('Informe um numero1: '));
+
+let maior = numero1;
+
+let numero 2 = parseFloat(prompt('Informe um numero2: '));
+if (numero2 > maior){
+    maior = numero2;
+}
+let numero 3 = parseFloat(prompt('Informe um numero3: '));
+if (numero3 > maior){
+    maior = numero3;
+}
+
+alert(maior);
+
+
+
+*/
+
 let numero1 = parseFloat(prompt('Entre com o numero 1: '));
 let numero2 = parseFloat(prompt('Entre com o numero 2: '));
 let numero3 = parseFloat(prompt('Entre com o numero 3: '));
