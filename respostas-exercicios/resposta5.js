fba4a281a2f8f13f3731959a75d109f385ab05fe
Mateus <mateus_sat@hotmail.com>
2019-07-04 19:41:22 -0300

    separado os valores dentro do isNaN

--- respostas-exercicios/resposta5.js
+++ respostas-exercicios/resposta5.js
@@ -6,13 +6,12 @@ A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
 */
 
-
 let nota1 = parseFloat(prompt('Digite Nota 1: '));
 let nota2 = parseFloat(prompt('Digite Nota 2: '));
 
 let media = (nota1 + nota2) / 2; // faz a media entre nota 1 e 2
 
-if (isNaN(nota1, nota2) || media < 0 || media > 10) { // o isNaN não aceita mais de um valor
+if (isNaN(nota1) || isNaN(nota2) || media < 0 || media > 10) { // o isNaN não aceita mais de um valor:isNaN(nota1,nota2)
     alert('Entrada Invalida!');
 } else if (media == 10) {
     alert('Aprovado com Distinção!');
