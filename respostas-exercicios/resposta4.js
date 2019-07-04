31238ccd14600bdea27d104209513de9cb570ba1
Mateus <mateus_sat@hotmail.com>
2019-07-04 19:28:20 -0300

    adicionando comentarios

--- respostas-exercicios/resposta4.js
+++ respostas-exercicios/resposta4.js
@@ -1,7 +1,7 @@
 
 //Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
-let escrita = prompt('Entre com uma letra: ').toLowerCase().trim(); // o toLowerCase() precisa dos parenteses pois é um metodo
+let escrita = prompt('Entre com uma letra: ').toLowerCase().trim(); // o toLowerCase() precisa dos parenteses pois é um metodo eo trim tira os espaços desnecessarios
 
 if (escrita == 'a' || escrita == 'e' || escrita == 'i' || escrita == 'o' || escrita == 'u') {
 
