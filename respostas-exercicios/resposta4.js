12f9d8a7ff0c0dc7949d3742d38937ebfd78f440
Mateus <mateus_sat@hotmail.com>
2019-07-04 19:35:30 -0300

    comentarios

--- respostas-exercicios/resposta4.js
+++ respostas-exercicios/resposta4.js
@@ -3,6 +3,20 @@
 
 let escrita = prompt('Entre com uma letra: ').toLowerCase().trim(); // o toLowerCase() precisa dos parenteses pois é um metodo eo trim tira os espaços desnecessarios
 
+/*
+    se eu colocar: 
+
+    let resposta = 'a letra é uma consoante'
+
+    ela ja comecaria sendo uma consoante e eliminaria o else do programa abaixo
+
+    if (escrita == 'a' || escrita == 'e' || escrita == 'i' || escrita == 'o' || escrita == 'u') {
+
+    alert('A letra digitada é uma Vogal!');
+    }
+
+*/
+
 if (escrita == 'a' || escrita == 'e' || escrita == 'i' || escrita == 'o' || escrita == 'u') {
 
     alert('A letra digitada é uma Vogal!');
